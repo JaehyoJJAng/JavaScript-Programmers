0e7854aec6c51b5b6ddccf8d13a9be7891eb8d98
JaehyoJJAng <yshrim12@naver.com>
2022-12-29 16:02:10 +0900

    숫자비교하기

---
+++ practice/숫자비교하기.js
@@ -0,0 +1,20 @@
+{
+  // 풀이 (1)
+  const solution = (num1,num2) => {
+    if (num1 === num2) {
+      return 1
+    }
+    else {
+      return -1
+    }
+  }
+}
+
+{
+  // 풀이 (2)
+  const solution = (num1,num2) => {
+    var answer = num1 === num2 ? 1 : -1;
+    return answer
+  };
+  console.log(solution(num1=1,num2=1))
+}
